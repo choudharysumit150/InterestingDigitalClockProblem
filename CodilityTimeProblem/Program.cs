2f8e402c821271e27c9b9c6aaf35689130be3af5
=Sumit Choudhary <choudharysumit150@gmail.com>
2019-08-04 22:37:23 +0530

    time parse , string to Time spam

--- CodilityTimeProblem/Program.cs
+++ CodilityTimeProblem/Program.cs
@@ -10,7 +10,7 @@ namespace CodilityTimeProblem
     {
         static void Main(string[] args)
         {
-            int result = Solution.solution("", "");
+            int result = Solution.solution("15:15:00", "15:15:12");
             Console.ReadLine();
         }
     }
@@ -19,6 +19,16 @@ namespace CodilityTimeProblem
 
         public static int solution(String S, String T)
         {
+            TimeSpan startTime = TimeSpan.Parse(S);
+            Console.WriteLine("Start Time : " + startTime.ToString());
+            TimeSpan endTime = TimeSpan.Parse(T);
+            TimeSpan oneSecond = TimeSpan.Parse("00:00:01");
+            startTime = startTime.Add(oneSecond);
+            Console.WriteLine("Start Time : " + startTime.ToString());
+            if (startTime< endTime)
+            {
+                Console.WriteLine("S is less than T");
+            }
             return 0;
         }
     }
